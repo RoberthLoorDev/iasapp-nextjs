706d2d80d7d938c70c2d715dc7aa44c1a4480a27
RoberthLoorDev <dayvidlife@gmail.com>
2025-08-16 18:03:52 -0500

    add: firts configuration

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function page() {
+     return (
+          <div>
+               <h1>Welcome to the Home Page</h1>
+               <p>This is the main content area.</p>
+          </div>
+     );
+}
