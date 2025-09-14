a093bc38296a15972d4c9355b8ca343090840fef
RoberthLoorDev <dayvidlife@gmail.com>
2025-09-14 02:07:35 -0500

    fix: ajustar el estilo del layout principal para mejorar la presentación del título

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -10,8 +10,8 @@ export default function MainLayout({ children, title }: MainLayoutProps) {
      return (
           <div className="w-full min-h-screen flex">
                <SlideBarC />
-               <main className="flex-1 p-4 overflow-x-hidden">
-                    {title && <h1 className="text-2xl font-semibold text-[#04452E] mb-4">{title}</h1>}
+               <main className="flex-1 p-5 overflow-x-hidden mt-[20px]">
+                    {title && <h1 className="text-3xl font-semibold text-[#04452E] mb-4">{title}</h1>}
                     {children}
                </main>
           </div>
