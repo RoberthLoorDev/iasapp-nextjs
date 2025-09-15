c65fec97dadbd74e3af7bb6c336f0ca1611547c8
RoberthLoorDev <dayvidlife@gmail.com>
2025-09-14 20:40:39 -0500

    fix: ajustar margen izquierdo en el layout principal para mejorar la presentación

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -10,7 +10,7 @@ export default function MainLayout({ children, title }: MainLayoutProps) {
      return (
           <div className="w-full min-h-screen flex">
                <SlideBarC />
-               <main className="flex-1 p-5 overflow-x-hidden mt-[20px]">
+               <main className="flex-1 p-5 overflow-x-hidden mt-[20px] ml-[90px]">
                     {title && <h1 className="text-3xl font-semibold text-[#04452E] mb-4">{title}</h1>}
                     {children}
                </main>
